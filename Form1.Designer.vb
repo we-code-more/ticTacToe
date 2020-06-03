1c41fc7a6449cb4ce3a260a370a20599f71f2196
austin <austin@DESKTOP-10TRAKH>
2020-06-03 10:00:01 -0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTicTacToe = New System.Windows.Forms.Label()
+        Me.Grpbox = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Grpbox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTicTacToe
+        '
+        Me.lblTicTacToe.BackColor = System.Drawing.Color.Gray
+        Me.lblTicTacToe.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTicTacToe.ForeColor = System.Drawing.Color.Gray
+        Me.lblTicTacToe.Location = New System.Drawing.Point(48, 56)
+        Me.lblTicTacToe.Name = "lblTicTacToe"
+        Me.lblTicTacToe.Size = New System.Drawing.Size(155, 108)
+        Me.lblTicTacToe.TabIndex = 0
+        '
+        'Grpbox
+        '
+        Me.Grpbox.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Grpbox.Controls.Add(Me.Label8)
+        Me.Grpbox.Controls.Add(Me.Label7)
+        Me.Grpbox.Controls.Add(Me.Label6)
+        Me.Grpbox.Controls.Add(Me.Label5)
+        Me.Grpbox.Controls.Add(Me.Label4)
+        Me.Grpbox.Controls.Add(Me.Label2)
+        Me.Grpbox.Controls.Add(Me.Label3)
+        Me.Grpbox.Controls.Add(Me.Label1)
+        Me.Grpbox.Controls.Add(Me.lblTicTacToe)
+        Me.Grpbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Grpbox.Location = New System.Drawing.Point(31, 90)
+        Me.Grpbox.Name = "Grpbox"
+        Me.Grpbox.Size = New System.Drawing.Size(719, 559)
+        Me.Grpbox.TabIndex = 1
+        Me.Grpbox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Gray
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.ForeColor = System.Drawing.Color.Gray
+        Me.Label1.Location = New System.Drawing.Point(273, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(155, 108)
+        Me.Label1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Gray
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.ForeColor = System.Drawing.Color.Gray
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.BottomRight
+        Me.Label2.Location = New System.Drawing.Point(483, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(155, 108)
+        Me.Label2.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Gray
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.ForeColor = System.Drawing.Color.Gray
+        Me.Label3.Location = New System.Drawing.Point(48, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(155, 108)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Gray
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.ForeColor = System.Drawing.Color.Gray
+        Me.Label4.Location = New System.Drawing.Point(273, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 108)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Gray
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.ForeColor = System.Drawing.Color.Gray
+        Me.Label5.Location = New System.Drawing.Point(483, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(155, 108)
+        Me.Label5.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Gray
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label6.ForeColor = System.Drawing.Color.Gray
+        Me.Label6.Location = New System.Drawing.Point(48, 379)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(155, 115)
+        Me.Label6.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Gray
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label7.ForeColor = System.Drawing.Color.Gray
+        Me.Label7.Location = New System.Drawing.Point(273, 379)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(155, 108)
+        Me.Label7.TabIndex = 6
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.Gray
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label8.ForeColor = System.Drawing.Color.Gray
+        Me.Label8.Location = New System.Drawing.Point(483, 379)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(155, 108)
+        Me.Label8.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Tahoma", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.Label9.Location = New System.Drawing.Point(274, 22)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(206, 48)
+        Me.Label9.TabIndex = 2
+        Me.Label9.Text = "Tictactoe"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 646)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Grpbox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Grpbox.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTicTacToe As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Grpbox As GroupBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label9 As Label
+End Class
